c287f9d33d34e4bedce511b9868dbe227c672372
Christine Roethel <roethel.christine@gmail.com>
2021-11-08 17:10:58 -0500

    exercise 4 complete

--- answers/Part 4.py
+++ answers/Part 4.py
@@ -7,3 +7,15 @@
 # as a ranking of strongest to weakest trainer. You may interpret strongest in whatever way you want,
 # but you will have to explain your decision.
 
+
+SELECT pokemons_name, trainers_name, current_level, primary_type_name, types.name AS secondary_type_name
+FROM (select pokemons.name as pokemons_name, trainers.trainername as trainers_name, pokemon_trainer.pokelevel as current_level, types.name as primary_type_name, pokemons.secondary_type as secondary_type_id
+FROM pokemons
+JOIN pokemon_trainer ON pokemon_trainer.pokemon_id = pokemons.id
+JOIN trainers on trainers.trainerid = pokemon_trainer.trainerid
+JOIN types on types.id = pokemons.primary_type) table_1
+JOIN types on types.id = secondary_type_id
+ORDER BY current_level desc
+
+
+# Ordered by level
